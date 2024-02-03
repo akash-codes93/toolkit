a6a29db7f2b31ede12ececf2ee5a9fe8822901ad
akashgupta <akash5gupta5@gmail.com>
2024-02-03 19:20:49 +0530

    v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/akash-codes93/toolkit/v2
+
+go 1.21.5
---
+++ v2/tools_test.go
@@ -0,0 +1,149 @@
+package toolkit
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"mime/multipart"
+	"net/http/httptest"
+	"os"
+	"sync"
+	"testing"
+)
+
+func TestTools_RandomString(t *testing.T) {
+	var testTools Tools
+
+	s := testTools.RandomString(10)
+	if len(s) != 10 {
+		t.Error("length is not right")
+	}
+}
+
+var uploadTests = []struct {
+	name          string
+	allowedTypes  []string
+	renameFile    bool
+	errorExpected bool
+}{
+	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false},
+	{name: "allowed rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: true, errorExpected: false},
+	{name: "not allowed", allowedTypes: []string{"image/png"}, renameFile: true, errorExpected: true},
+}
+
+func TestTools_UploadFiles(t *testing.T) {
+	for _, e := range uploadTests {
+		// setup a pipe to avoid buffering
+		pr, pw := io.Pipe()
+		writer := multipart.NewWriter(pw)
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		go func() {
+			defer writer.Close()
+			defer wg.Done()
+
+			// create the form data filed "file"
+			part, err := writer.CreateFormFile("file", "./testdata/img.jpeg")
+			if err != nil {
+				t.Error(err)
+			}
+
+			f, err := os.Open("./testdata/img.jpeg")
+			if err != nil {
+				t.Error(err)
+			}
+			defer f.Close()
+			img, _, err := image.Decode(f)
+			if err != nil {
+				t.Error(err)
+			}
+			quality := jpeg.Options{
+				Quality: 50,
+			}
+			err = jpeg.Encode(part, img, &quality)
+			if err != nil {
+				t.Error(err)
+			}
+		}()
+		// read from the pipe which recieves data
+		request := httptest.NewRequest("POST", "/", pr)
+		request.Header.Add("Content-Type", writer.FormDataContentType())
+
+		var testTools Tools
+		testTools.AllowedFileTypes = e.allowedTypes
+		uploadedFiles, err := testTools.UploadedFiles(request, "./testdata/uploads/", e.renameFile)
+		if err != nil && !e.errorExpected {
+			t.Error(err)
+		}
+
+		if !e.errorExpected {
+			if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)); os.IsNotExist(err) {
+				t.Errorf("%s expected the file to exists: %s", e.name, err.Error())
+			}
+			_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName))
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s expected but non recieved", e.name)
+
+		}
+		wg.Wait()
+
+	}
+}
+
+func TestTools_CreateDirIfNotExists(t *testing.T) {
+	var testTools Tools
+
+	err := testTools.CreateDirIfNotExists("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testTools.CreateDirIfNotExists("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ = os.Remove("./testdata/myDir")
+
+}
+
+// slice of struct test data
+var SlugifyTestData = []struct {
+	s             string
+	errorExpected bool
+	slug          string
+}{
+	{s: "this is a tool", errorExpected: false, slug: "this-is-a-tool"},
+	{s: "this?? !! is a tool", errorExpected: false, slug: "this-is-a-tool"},
+	{s: "", errorExpected: true, slug: ""},
+	{s: "???", errorExpected: true, slug: ""},
+}
+
+func TestTools_Slugify(t *testing.T) {
+
+	var testTools Tools
+
+	for _, e := range SlugifyTestData {
+
+		slug, err := testTools.Slugify(e.s)
+		if err != nil && !e.errorExpected {
+			t.Error(err)
+		}
+
+		if !e.errorExpected {
+			if slug != e.slug {
+				t.Errorf("%s expected %s but recieved %s", e.s, e.slug, slug)
+			}
+		}
+		if e.errorExpected && err == nil {
+			t.Errorf("%s expected but non recieved", e.s)
+		}
+
+	}
+
+}
